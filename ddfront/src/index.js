0da4d8ceb32b62fff19d695e02caa79254116edc
Lee Pierson <awesomehenge@gmail.com>
2023-08-07 10:22:10 +0100

    up04

--- ddfront/src/index.js
+++ ddfront/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+<Auth0Provider
+    domain="dev-mqz0g5ehf8p0rbx7.us.auth0.com"
+    clientId="hDuP7ucLcAR4MYjrwNkcsgyuN2yVcTWy"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>,
 );
-
-
