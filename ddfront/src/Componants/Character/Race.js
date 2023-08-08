70a7ce51b75939097b3b04a04694076de4f1ed66
Lee Pierson <awesomehenge@gmail.com>
2023-08-08 14:19:51 +0100

    up08

--- ddfront/src/Componants/Character/Race.js
+++ ddfront/src/Componants/Character/Race.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
 function MyRace(){
+    const [race, setRace] = useState("");
     return(
     <form>
             <select>
-                 <option value=>Pick your Race</option>
+                <option value=>Pick your Race</option>
                 <option value=>Vampire</option>
-                 <option value=>Warewolf</option>
-                 <option value=>Witch</option>
-                 <option value=>Zombie</option>
-                 </select>
+                <option value=>Warewolf</option>
+                <option value=>Witch</option>
+                <option value=>Zombie</option>
+                onChange={(e) => setRace(e.target.value)}
+            </select>
         </form>
     )
 };
