7d256696664bd7cc76344457ebbbdcf3f5443193
Lee Pierson <awesomehenge@gmail.com>
2023-08-09 13:32:13 +0100

    update15

---
+++ ddfront/src/components/Form/Form.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+export default function Form({ onSubmitFunc, sheet }) {
+  const [forData, setFormData] = useState(
+    sheet ?? {
+      Name: "",
+      Race: "",
+      Role: "",
+      Alignment: "",
+    }
+  );
+
+  const handleChange = (event) => {
+    setFormData({ ...FormData, [event.target.Name]: event.target.value });
+    console.log(formData);
+  };
+
+  const submit = (event) => {
+    event.preventDefault();
+    onSubmitFunc(forData);
+  };
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <input
+          placeholder="Character Name"
+          onChange={handleChange}
+          Name="Name"
+          value={formData.Name}
+        ></input>
+        <input
+          placeholder="Character Race"
+          onChange={handleChange}
+          Name="Race"
+          value={formData.Race}
+        ></input>
+        <input
+          placeholder="Character Role"
+          onChange={handleChange}
+          Name="Role"
+          value={formData.Role}
+        ></input>
+        <input
+          placeholder="Character Alignment"
+          onChange={handleChange}
+          Name="Alignment"
+          value={formData.Alignment}
+        ></input>
+      </form>
+    </div>
+  );
+}
--- ddfront/src/components/SheetCard/SheetCard.js
+++ ddfront/src/components/SheetCard/SheetCard.js
@@ -1,1 +1,23 @@
-import
+import React from "react";
+import Form from "../Form/Form";
+
+export default function SheetCard({ sheet, handleDelete, handleUpdateSheet }) {
+  return (
+    <div>
+      {sheet.map((sheet) => {
+        return (
+          <div key={sheet.id}>
+            <div>
+              <p onClick={() => handleDelete(sheet._id)}>x</p>
+              <h3>{sheet.Name}</h3>
+              <p>
+                {sheet.Race}, {sheet.Role}, {sheet.Alignment}
+              </p>
+            </div>
+            <Form onSubmitFunc={handleUpdateSheet} sheet={sheet} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
